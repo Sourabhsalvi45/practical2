7b4f73beba07dfd3ea0c06801e51b13ae13dd431
Vansh3006 <100557683+Vansh3006@users.noreply.github.com>
2022-09-15 00:05:05 +0530

    COMPUTER GRAPHICS: Practical 6: FixUP! Fix Program CG_6.C

--- Semester III/COMPUTER GRAPHICS/Practical 6/CG_6.C
+++ Semester III/COMPUTER GRAPHICS/Practical 6/CG_6.C
@@ -1,50 +1,52 @@
 //Experiment 6: Scanline Polygon Fill Algorithm
 
-#include<stdio.h>
-#include<conio.h>
-#include<graphics.h>
-#include<dos.h>
+#include <stdio.h>
+#include <conio.h>
+#include <graphics.h>
+#include <dos.h>
 
-void main()
+void main ()
 {
- int i,j,k,gd=DETECT,gm,temp,n,y;
- float m[10],a[10][2],x[10];
+ int gd=DETECT, gm, i, j, k, n;
+ float a[10][2], x[10], m[10], temp, y;
  printf("Enter number of vertices: ");
- scanf("%d",&n);
- for(i=0;i<n;i++)
- {
-  printf("Enter x and y coordinate of point no %d: ", i+1);
-  scanf("%f%f",&a[i][0],&a[i][1]);
- }
- a[n][0]=a[0][0];
- a[n][1]=a[0][1];
- initgraph(&gd,&gm,"c:\\turboc3\\bgi");
+ scanf("%d", &n);
+ printf("\n");
  for(i=0; i<n; i++)
  {
-  line(a[i][0],a[i][1],a[i+1][0],a[i+1][1]);
+  printf("Enter x and y co-ordinates of point %d: ", i+1);
+  scanf("%f%f", &a[i][0], &a[i][1]);
  }
+ a[n][0] = a[0][0];
+ a[n][1] = a[0][1];
+
+ initgraph(&gd, &gm, "c:\\turboc3\\bgi");
+
+ for(i=0; i<n; i++)
+	line(a[i][0], a[i][1], a[i+1][0], a[i+1][1]);
  getch();
 
  for(i=0; i<n; i++)
  {
-  if(a[i][0]==a[i+1][0]) m[i]=0;
-  else if(a[i][1]==a[i+1][1]) m[i]=1;
-  else m[i]=(a[i][0]-a[i+1][0])/(a[i][1]-a[i+1][1]);
+  if(a[i][1] == a[i+1][1]) m[i] = 1;
+  else m[i] = (a[i][0] - a[i+1][0])/(a[i][1] - a[i+1][1]);
  }
-for(y=0;y<480;y++)
-{
- k=0;
- for(i=0; i<n; i++)
+ //Finding X intersections
+ for(y=0; y<480; y++)
  {
-  if((a[i][1]>y && a[i+1][1]<=y) || (a[i+1][1]>y && a[i][1]<=y))
+  k=0;
+  for(i=0; i<n; i++)
   {
-   x[k]=a[i][0]+(y-a[i][1])*m[i];
-   k++;
+   if(((a[i][1] > y) && (a[i+1][1] <= y)) || ((a[i][1] <= y) && (a[i+1][1] > y)))
+   {
+    x[k] = a[i][0] + m[i]*(y - a[i][1]);
+    k++;
+   }
   }
- }
-  for(i=0; i<k; i++)
+  //Sorting x intersection points
+  for(i=0; i<k-1; i++)
   {
-   for(j=0; j<k; j++)
+   for(j=0; j<k-i-1; i++)
    {
     if(x[j] > x[j+1])
     {
@@ -54,13 +56,13 @@ for(y=0;y<480;y++)
     }
    }
   }
+  setcolor(4);
+  //Pairing and drawing lines
   for(i=0; i<k; i=i+2)
   {
-   line(x[i]+1, y, (x[i+1]-1), y);
-   setcolor(4);
+   line((x[i]+1), y, (x[i+1]-1), y);
    delay(10);
   }
  }
  getch();
 }
-
